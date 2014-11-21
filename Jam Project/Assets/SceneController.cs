003070d31684ba31eb7fcaf1ccd10a59fa972e64
JeremyrDev <Jeremyr.dev@gmail.com>
2014-11-21 03:45:09 -0500

    Spawn pool, waypoint system, etc.

---
+++ Jam Project/Assets/SceneController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class SceneController : MonoBehaviour 
+{
+	//Object pool
+	public GameObject s;
+	List<GameObject> sheep;
+
+	//Parent objects
+	public GameObject Sheep;
+	public GameObject Cow;
+	public GameObject Goat;
+
+	public int houseFoodLevel = 100;
+	int houseFoodCapacity = 100;
+	int numberOfSheep = 20;
+
+	float spawnTimer = 0;
+	float timerLimit = 1;
+
+	string info;
+
+	public GUIStyle infoStyle;
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (0,0,100,50), info, infoStyle);
+	}
+
+	void Start () 
+	{
+		sheep = new List<GameObject>();
+		
+		for(int i = 0; i < numberOfSheep; i++)
+		{
+			GameObject obj = (GameObject)Instantiate(s);
+			obj.SetActive(false);
+			obj.name = "sheep"+i;
+			obj.transform.parent = Sheep.transform;
+			sheep.Add(obj); 
+		}
+	}
+
+	void Update () 
+	{
+		info = "Spawn: " + spawnTimer + "\n" + "Food level: " + houseFoodLevel;
+		spawnTimer += 1*Time.deltaTime;
+		if(spawnTimer > timerLimit)
+		{
+			spawnTimer = 0;
+			spawnAnimal("s");
+		}
+		if(houseFoodLevel <= 0)
+		{
+			info = "Game Over!";
+			Time.timeScale = 0;
+		}
+	}
+	public void spawnAnimal(string type)
+	{
+		if(type == "s")
+		{
+			int counter = 0;
+			for(int i = 0; i < numberOfSheep;i++)
+			{
+				if(!sheep[i].activeInHierarchy)
+				{
+					sheep[i].transform.position = new Vector3(-21.2f, 1f, Random.Range(7.1f, 9f));
+					sheep[i].SetActive(true);
+					return;
+				}
+				counter++;
+				if(counter == numberOfSheep)
+				{
+					GameObject obj = (GameObject)Instantiate(s);
+					obj.SetActive(true);
+					obj.name = "sheep"+(i+1);
+					obj.transform.parent = Sheep.transform;
+					sheep.Add(obj);
+					numberOfSheep++;
+					sheep[i+1].transform.position = new Vector3(-21.2f, 1f, Random.Range(7.1f, 9f));
+					return;
+				}
+			}
+		}
+	}
+}
---
+++ Jam Project/Assets/SheepScript.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SheepScript : MonoBehaviour 
+{
+	Vector3 endPos;
+	
+	public float maxHealth = 100;
+	public float health = 100;
+	float speed = 10;
+	float spawnTimer = 0;
+	
+	int wayPointCounter = 0;
+	
+	
+	public bool target = false;
+	
+	//public Material enemy;
+	//public Material enemyTarget;
+	
+	public GameObject c;
+	//public GameObject healthBar;
+	
+	SceneController sc;
+	
+	void OnEnable()
+	{
+		wayPointCounter = 0;
+		transform.position = new Vector3(-21.2f, 1f, Random.Range(7.1f, 9f));
+		endPos = new Vector3(-17.1f, 1f, 8.1f);
+		maxHealth+=10;
+		health = maxHealth;
+		//healthBar.transform.localScale = new Vector3(.1f, .14f, .009f);
+	}
+
+	void Start () 
+	{
+		c = GameObject.Find("Camera");
+		sc = c.GetComponent<SceneController>();
+		transform.position = new Vector3(-21.2f, 1f, Random.Range(7.1f, 9f));
+		endPos = new Vector3(-17.1f, 1f, 8.1f);
+	}
+
+	public void ApplyDamage(float damage)
+	{
+		health-=damage;
+	}
+
+	void Update () 
+	{
+		float h = health/maxHealth;
+		
+		//healthBar.transform.localScale = new Vector3(h/10, .14f, .009f);
+		
+		if(health <=0){
+			gameObject.SetActive(false);
+			//sc.killCounter++;
+		}
+		
+		transform.position = Vector3.MoveTowards(transform.position, endPos, Time.deltaTime * speed);
+		//rotate towards waypoint
+		transform.LookAt(endPos);
+		if(wayPointCounter == 0){
+			if(transform.position == endPos)
+			{
+				wayPointCounter++;
+				endPos = new Vector3(.23f, 1f, 8.1f);
+
+			}
+		}
+		if(wayPointCounter == 1){
+			if(transform.position == endPos)
+			{
+				wayPointCounter++;
+				endPos = new Vector3(.23f, 1f, -.86f);
+			}
+		}
+		if(wayPointCounter == 2){
+			if(transform.position == endPos)
+			{
+				wayPointCounter++;
+				endPos = new Vector3(-9.1f, 1f, -.86f);
+			}
+		}
+		if(wayPointCounter == 3){
+			if(transform.position == endPos)
+			{
+				wayPointCounter++;
+				endPos = new Vector3(-9.1f, 1f, 4.3f);
+			}
+		}
+		if(wayPointCounter == 4){
+			if(transform.position == endPos)
+			{
+				wayPointCounter++;
+				endPos = new Vector3(-16.4f, 1f, 4.3f);
+			}
+		}
+		if(wayPointCounter == 5){
+			if(transform.position == endPos)
+			{
+				wayPointCounter++;
+				endPos = new Vector3(-16.4f, 1f, -6.1f);
+			}
+		}
+		if(wayPointCounter == 6){
+			if(transform.position == endPos)
+			{
+				wayPointCounter++;
+				endPos = new Vector3(7.7f, 1f, -6.1f);
+			}
+		}
+		if(wayPointCounter == 7){
+			if(transform.position == endPos)
+			{
+				wayPointCounter++;
+				endPos = new Vector3(7.7f, 1f, 4.5f);
+			}
+		}
+		if(wayPointCounter == 8){
+			if(transform.position == endPos)
+			{
+				wayPointCounter++;
+				endPos = new Vector3(13.6f, 1f, Random.Range(3.5f, 5.5f));
+			}
+		}
+		if(wayPointCounter == 9){
+			if(transform.position == endPos)
+			{
+				//decrease house food capacity
+
+				sc.houseFoodLevel--;
+
+				//gameObject.SetActive(false);
+			}
+		}
+	}
+}
